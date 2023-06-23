c7b347c8f76287c80b0ff1d7d9778a5dcf158a12
Kukks <evilkukka@gmail.com>
2023-06-23 15:10:44 +0200

    wip

--- BTCPayServer/Hosting/BTCPayServerServices.cs
+++ BTCPayServer/Hosting/BTCPayServerServices.cs
@@ -515,6 +515,8 @@ o.GetRequiredService<IEnumerable<IPaymentLinkExtension>>().ToDictionary(o => o.P
                     });
             }
 
+            services.AddSingleton<BtcPayAppService>();
+
             return services;
         }
 
